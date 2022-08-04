ef9e45c5df543b50e57b3afa642fd8aef5b08bad
Ubuntu <ubuntu@ip-172-31-2-146.us-west-2.compute.internal>
2022-08-04 11:56:31 +0000

    Success of favorite finction

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :poems do
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 end
