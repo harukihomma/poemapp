0253d7e84bade6ceffbcfdc9db2841d01f73327a
Ubuntu <ubuntu@ip-172-31-2-146.us-west-2.compute.internal>
2022-08-04 13:51:23 +0000

    Change home path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "home#top"
+  get 'home', to: 'home#index'
   resources :poems do
     resource :favorites, only: [:create, :destroy]
   end
